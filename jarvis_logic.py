f04f4ef7511e89a1eeaebc4cc55cdc92ded9acdf
Horizon899 <gaelcasares899@gmail.com>
2024-12-05 05:39:18 -0600

    .

--- jarvis_logic.py
+++ jarvis_logic.py
@@ -1,1 +1,13 @@
 import speech_recognition as sr
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Bienvenido Gael")
+    audio = r.listen(source)
+
+try:
+    print("Usted dijo " + r.recognize_sphinx(audio))
+except sr.UnknownValueError:
+    print("Perdón señor no entendí lo que me dijo")
+except sr.RequestError as e:
+    print("Error fatal papu")
